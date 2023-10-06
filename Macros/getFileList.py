f8647daaa71f385440440eb629d2b8f3cf9a328a
chizhang <chi.zhang@cern.ch>
2023-10-06 12:04:22 +0200

    add macros for analysis

---
+++ Macros/getFileList.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import alienpy.alien as alien
+
+""" 
+---- How to run:
+python3 getFileList.py -r 505713 -y 2021 -p NOV -u arakotoz
+"""
+
+def getRunFileList(Input, Output):
+    alien.setup_logging()
+    jal = alien.AliEn()
+    findCmd = "find {} {} -e AOD".format(Input, "AO2D.root")
+    print(findCmd)
+    out = jal.run(findCmd)
+
+    results = out.ansdict['results']
+    with open(Output, "a") as outFile:
+        for dic in results:
+            outFile.write("alien://{}\n".format(dic["lfn"]))
+
+    return out.exitcode
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get file lists")
+    parser.add_argument("--Input", "-i", help="Directory to input files", required=True)
+    parser.add_argument("--Output", "-o", help="Directory to output files", default="./AODList.txt")
+
+    args = parser.parse_args()
+    getRunFileList(args.Input, args.Output)
