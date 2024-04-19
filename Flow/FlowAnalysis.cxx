469e2c309869428c6d166bdd2311729905a41232
czhang <chi.zhang@cern.ch>
2024-04-19 08:57:18 +0200

    Update FlowAnalysis.cxx

--- Flow/FlowAnalysis.cxx
+++ Flow/FlowAnalysis.cxx
@@ -15,6 +15,9 @@
 #include <TMatrixD.h>
 #include <TPad.h>
 #include <TParameter.h>
+#include <TProfile.h>
+#include <TProfile2D.h>
+#include <TProfile3D.h>
 #include <TROOT.h>
 #include <TString.h>
 #include <TStyle.h>
@@ -29,54 +32,43 @@
 #include "Framework/Logger.h"
 
 using namespace std;
+
 void LoadData(TChain *fChain, std::string TreeName = "O2rerefflow",
               std::string FileName = "AO2D.root");
 void CreateBins(float *axis, float min, float max, int Nbins = 10);
 
-void FlowAnalysis(float dimuonMassMin = 0., float dimuonMassMax = 4.,
-                  float dimuonPtMin = 0., float dimuonPtMax = 10.,
-                  bool fPropError = true, bool fCumulant = true) {
+void FlowAnalysis(std::array<float, 3> dimuonMassRange,
+                  std::array<float, 3> dimuonPtRange,
+                  std::array<float, 2> dimuonCentRange, bool fPropError = true,
+                  bool fCumulant = true) {
 
   TChain *fChain_REF = new TChain();
   if (fCumulant) {
     LoadData(fChain_REF, "O2rerefflow");
   }
-
   TChain *fChain_POI = new TChain();
   LoadData(fChain_POI, "O2rtdimuonall");
 
+  int NBinsMass = static_cast<int>(dimuonMassRange[2]);
+  int NBinsPt = static_cast<int>(dimuonPtRange[2]);
   int NBinsMult = 10;
-  int NBinsMass = 20;
-  int NBinsPt = 20;
   float Cent[11] = {0., 5., 10., 20., 30., 40., 50., 60., 70., 80., 90.};
   float *MassBins = new float[NBinsMass + 1];
   float *PtBins = new float[NBinsPt + 1];
 
-  CreateBins(MassBins, dimuonMassMin, dimuonMassMax, NBinsMass);
-  CreateBins(PtBins, dimuonPtMin, dimuonPtMax, NBinsPt);
+  CreateBins(MassBins, dimuonMassRange[0], dimuonMassRange[1], NBinsMass);
+  CreateBins(PtBins, dimuonPtRange[0], dimuonPtRange[1], NBinsPt);
 
   //////////////////////////////////////////////////////////////////////////////////////
   // Post-processing of reference flow for cumulants
   //////////////////////////////////////////////////////////////////////////////////////
-
-  float C22REF = 0.;  //! Event averaged 2-p cumulant for reference
-  float C24REF = 0.;  //! Event averaged 4-p cumulant for reference
-  float C22EREF = 0.; //! Error for event averaged 2-p cumulant for reference
-  float C24EREF = 0.; //! Error for event averaged 4-p cumulant for reference
-
   if (fCumulant) {
-    float C22MultREF[10] = {0.};
-    float C22EMultREF[10] = {0.};
-    float V22MultREF[10] = {0.};
-    float V22EMultREF[10] = {0.};
-    float C24MultREF[10] = {0.};
-    float C24EMultREF[10] = {0.};
-    float V24MultREF[10] = {0.};
-    float V24EMultREF[10] = {0.};
-    float Sum22REF[10] = {0.};
-    float Sum24REF[10] = {0.};
-    float Sum22REFAll = 0.;
-    float Sum24REFAll = 0.;
+
+    TProfile *Corr22Ref = new TProfile(
+        "Corr22Ref", "Profile of <2> with n=2 for ref", NBinsMult, Cent);
+    TProfile *Corr24Ref = new TProfile(
+        "Corr24Ref", "Profile of <4> with n=2 for ref", NBinsMult, Cent);
+
     float M11Ref;
     float M1111Ref;
     float Corr2Ref;
@@ -111,87 +103,56 @@ void FlowAnalysis(float dimuonMassMin = 0., float dimuonMassMax = 4.,
         continue;
       }
 
-      C22REF += Corr2Ref * M11Ref;
-      C24REF += Corr4Ref * M1111Ref;
-      C22EREF += pow(Corr2Ref, 2) * M11Ref;
-      C24EREF += pow(Corr4Ref, 2) * M1111Ref;
-      Sum22REFAll += M11Ref;
-      Sum24REFAll += M1111Ref;
-
-      // Centrality-differential correlations
-      for (int i = 0; i < NBinsMult; i++) {
-        if (CentFT0REF > Cent[i] && CentFT0REF <= Cent[i + 1]) {
-          C22MultREF[i] += Corr2Ref * M11Ref;
-          C24MultREF[i] += Corr4Ref * M1111Ref;
-          C22EMultREF[i] += pow(Corr2Ref, 2) * M11Ref;
-          C24EMultREF[i] += pow(Corr4Ref, 2) * M1111Ref;
-          Sum22REF[i] += M11Ref;
-          Sum24REF[i] += M1111Ref;
-        }
-      }
+      Corr22Ref->Fill(CentFT0REF, Corr2Ref, M11Ref);
+      Corr24Ref->Fill(CentFT0REF, Corr4Ref, M1111Ref);
     }
 
-    // Events averaged reference flow
-    C22REF = C22REF / Sum22REFAll;
-    C24REF = C24REF / Sum24REFAll - 2. * pow(C22REF, 2);
-    C22EREF = C22EREF / Sum22REFAll;
-    C22EREF = pow(C22EREF - C22REF * C22REF, 1. / 2);
-    C24EREF = C24EREF / Sum24REFAll;
-    C24EREF = pow(C24EREF - C24REF * C24REF, 1. / 2);
-    C24EREF = pow(C24EREF * C24EREF + 16. * C22EREF * C22EREF * C22REF * C22REF,
-                  1. / 2);
+    // Events averaged reference flow in given centrality range
+    TProfile Corr22RefAll;
+    Corr22Ref->Copy(Corr22RefAll);
+    TProfile Corr24RefAll;
+    Corr24Ref->Copy(Corr24RefAll);
+    // Rebin thep profile to cover centrality range for differential study
+    Corr22RefAll.SetBins(1, dimuonCentRange[0], dimuonCentRange[1]);
+    Corr24RefAll.SetBins(1, dimuonCentRange[0], dimuonCentRange[1]);
+    // Evaluate v2 in centrality range
+    float cor2All = Corr22RefAll.GetBinContent(1);
+    float cor2eAll = Corr22RefAll.GetBinError(1);
+    float v22All = cor2All < 0 ? 0. : pow(cor2All, 1. / 2);
+    float v22eAll = cor2All < 0 ? 0. : 0.5 * cor2eAll * pow(cor2All, 1. / 2);
+    float cor4All = Corr24RefAll.GetBinContent(1);
+    float cor4eAll = Corr24RefAll.GetBinError(1);
+    float c24All = cor4All - 2 * pow(cor2All, 2);
+    float c24eAll =
+        pow(cor4eAll * cor4eAll + 16 * cor2All * cor2All * cor2eAll * cor2eAll,
+            1. / 2);
+    float v24All = c24All > 0 ? 0. : pow(-c24All, 1. / 4);
+    float v24eAll = c24All > 0 ? 0. : pow(-c24All, (-3. / 4)) * c24eAll / 4;
 
-    // Centrality-dependent flow for reference
+    // Centrality-dependent reference flow
     for (int i = 0; i < NBinsMult; i++) {
+      // Fill v2{2}
+      float cor2 = Corr22Ref->GetBinContent(i + 1);
+      float cor2e = Corr22Ref->GetBinError(i + 1);
+      float v22 = cor2 < 0 ? 0. : pow(cor2, 1. / 2);
+      float v22e = cor2 < 0 ? 0. : 0.5 * cor2e * pow(cor2, 1. / 2);
+      hist_c22REF->SetBinContent(i + 1, cor2);
+      hist_c22REF->SetBinError(i + 1, cor2e);
+      hist_v22REF->SetBinContent(i + 1, v22);
+      hist_v22REF->SetBinError(i + 1, v22e);
 
-      if (Sum22REF[i] * Sum24REF[i] != 0.) {
-
-        C22MultREF[i] = C22MultREF[i] / Sum22REF[i];
-        C22EMultREF[i] = C22EMultREF[i] / Sum22REF[i];
-        C22EMultREF[i] =
-            pow(C22EMultREF[i] - C22MultREF[i] * C22MultREF[i], 1. / 2);
-
-        V22MultREF[i] = pow(C22MultREF[i], 1. / 2);
-        V22EMultREF[i] = 0.5 * C22EMultREF[i] * pow(C22MultREF[i], -1. / 2);
-
-        C24MultREF[i] =
-            C24MultREF[i] / Sum24REF[i] - 2. * pow(C22MultREF[i], 2);
-        C24EMultREF[i] = C24EMultREF[i] / Sum24REF[i];
-        C24EMultREF[i] =
-            C24EMultREF[i] - C24MultREF[i] * C24MultREF[i] > 0.
-                ? pow(C24EMultREF[i] - C24MultREF[i] * C24MultREF[i], 1. / 2)
-                : 0.;
-        C24EMultREF[i] = pow(C24EMultREF[i] * C24EMultREF[i] +
-                                 16. * C22MultREF[i] * C22MultREF[i] *
-                                     C22EMultREF[i] * C22EMultREF[i],
-                             1. / 2);
-
-        V24MultREF[i] = pow(-1. * C24MultREF[i], 1. / 4);
-        V24EMultREF[i] = C24MultREF[i] > 0 ? 0
-                                           : 1. / 4 * C24EMultREF[i] *
-                                                 pow(-C24MultREF[i], -3. / 4);
-
-        if (fPropError) {
-
-          hist_c22REF->SetBinContent(i + 1, C22MultREF[i]);
-          hist_c22REF->SetBinError(i + 1, C22EMultREF[i]);
-
-          hist_v22REF->SetBinContent(i + 1, V22MultREF[i]);
-          hist_v22REF->SetBinError(i + 1, V22EMultREF[i]);
-
-          hist_c24REF->SetBinContent(i + 1, C24MultREF[i]);
-          hist_c24REF->SetBinError(i + 1, C24EMultREF[i]);
-
-          hist_v24REF->SetBinContent(i + 1, V24MultREF[i]);
-          hist_v24REF->SetBinError(i + 1, V24EMultREF[i]);
-
-        } else {
-          hist_c22REF->SetBinContent(i + 1, C22MultREF[i]);
-          hist_v22REF->SetBinContent(i + 1, V22MultREF[i]);
-          hist_c24REF->SetBinContent(i + 1, C24MultREF[i]);
-          hist_v24REF->SetBinContent(i + 1, V24MultREF[i]);
-        }
-      }
+      // Fill v2{4}
+      float cor4 = Corr24Ref->GetBinContent(i + 1);
+      float cor4e = Corr24Ref->GetBinError(i + 1);
+      float c24 = cor4 - 2 * pow(cor2, 2);
+      float c24e =
+          pow(cor4e * cor4e + 16 * cor2 * cor2 * cor2e * cor2e, 1. / 2);
+      float v24 = c24 > 0 ? 0. : pow(-c24, 1. / 4);
+      float v24e = c24 > 0 ? 0. : pow(-c24, (-3. / 4)) * c24e / 4;
+      hist_c24REF->SetBinContent(i + 1, c24);
+      hist_c24REF->SetBinError(i + 1, c24e);
+      hist_v24REF->SetBinContent(i + 1, v24);
+      hist_v24REF->SetBinError(i + 1, v24e);
     }
 
     TCanvas *c1REF = new TCanvas("c22REF");
@@ -234,53 +195,34 @@ void FlowAnalysis(float dimuonMassMin = 0., float dimuonMassMax = 4.,
   }
 
   //////////////////////////////////////////////////////////////////////////////////////
-  // Post-processing for flow of POI
+  // Post-processing for POI flow
   //////////////////////////////////////////////////////////////////////////////////////
+  TProfile3D *Corr22Poi =
+      new TProfile3D("Corr22Poi", "Profile of <2> with n=2 for poi", NBinsMass,
+                     MassBins, NBinsPt, PtBins, NBinsMult, Cent);
+  TProfile3D *Corr24Poi =
+      new TProfile3D("Corr24Poi", "Profile of <4> with n=2 for poi", NBinsMass,
+                     MassBins, NBinsPt, PtBins, NBinsMult, Cent);
 
-  float *C22POI = new float[NBinsMass];
-  float *C22EPOI = new float[NBinsMass];
-  float *V22POI = new float[NBinsMass];
-  float *V22EPOI = new float[NBinsMass];
-  float *C24POI = new float[NBinsMass];
-  float *C24EPOI = new float[NBinsMass];
-  float *V24POI = new float[NBinsMass];
-  float *V24EPOI = new float[NBinsMass];
-  float *Sum22POI = new float[NBinsMass];
-  float *Sum24POI = new float[NBinsMass];
+  TProfile3D *U2Q2 = new TProfile3D("U2Q2", "U2Q2", NBinsMass, MassBins,
+                                    NBinsPt, PtBins, NBinsMult, Cent);
+  TProfile3D *R2SP = new TProfile3D("R2SP", "Resolution SP", NBinsMass,
+                                    MassBins, NBinsPt, PtBins, NBinsMult, Cent);
+  TProfile3D *Cos2DeltaPhi =
+      new TProfile3D("Cos2DeltaPhi", "Cos2DeltaPhi", NBinsMass, MassBins,
+                     NBinsPt, PtBins, NBinsMult, Cent);
+  TProfile3D *R2EP = new TProfile3D("R2EP", "Resolution EP", NBinsMass,
+                                    MassBins, NBinsPt, PtBins, NBinsMult, Cent);
 
-  float *V2SP = new float[NBinsMass];
-  float *V2ESP = new float[NBinsMass];
-  float *V2EP = new float[NBinsMass];
-  float *V2EEP = new float[NBinsMass];
-  float *V2MultBinMass = new float[NBinsMass];
-
-  for (int i = 0; i < NBinsMass; i++) {
-    C22POI[i] = 0.;
-    C22EPOI[i] = 0.;
-    C24POI[i] = 0.;
-    C24EPOI[i] = 0.;
-    V22POI[i] = 0.;
-    V22EPOI[i] = 0.;
-    V24POI[i] = 0.;
-    V24EPOI[i] = 0.;
-    Sum22POI[i] = 0.;
-    Sum24POI[i] = 0.;
-    V2SP[i] = 0.;
-    V2ESP[i] = 0.;
-    V2EP[i] = 0.;
-    V2EEP[i] = 0.;
-    V2MultBinMass[i] = 0.;
-  }
-
-  float M01Poi;
-  float M0111Poi;
-  float Corr2Poi;
-  float Corr4Poi;
+  float M01Poi = 0.;
+  float M0111Poi = 0.;
+  float Corr2Poi = 0.;
+  float Corr4Poi = 0.;
   float CentFT0POI;
   float fMass;
   float fPt, fPt1, fPt2;
   float fEta, fEta1, fEta2;
-  int fMultDimuons;
+  int fMultDimuons = 0.;
   float fU2Q2, fU3Q3, fCos2DeltaPhi, fCos3DeltaPhi;
   float fR2EP, fR2SP;
 
@@ -334,42 +276,29 @@ void FlowAnalysis(float dimuonMassMin = 0., float dimuonMassMax = 4.,
   // Loop over all dimuons
   for (int i = 0; i < fChain_POI->GetEntries(); i++) {
     fChain_POI->GetEntry(i);
-
     // Dimuons general selection
-    if (!(fPt > dimuonPtMin && fPt <= dimuonPtMax && fMass > dimuonMassMin &&
-          fMass <= dimuonMassMax && fEta > -4. && fEta < -2.5 &&
-          CentFT0POI > 0. && CentFT0POI <= 70.)) {
+    // {Pt range, msass bin, centrality range}
+    if (!(fPt > dimuonPtRange[0] && fPt <= dimuonPtRange[1] &&
+          fMass > dimuonMassRange[0] && fMass <= dimuonMassRange[1] &&
+          fEta > -4. && fEta < -2.5 && CentFT0POI > dimuonCentRange[0] &&
+          CentFT0POI <= dimuonCentRange[1])) {
       continue;
     }
-
-    // Fill bins for cumulants
+    // Fill (mass, pt, centrality) bins for cumulants
     if (fCumulant) {
       if (!(isnan(Corr2Poi) || isnan(Corr4Poi) || isinf(Corr2Poi) ||
             isinf(Corr4Poi))) {
-        for (int i = 0; i < NBinsMass; i++) {
-          if (fMass > MassBins[i] && fMass <= MassBins[i + 1]) {
-            C22POI[i] += Corr2Poi * M01Poi;
-            C24POI[i] += Corr4Poi * M0111Poi;
-            C22EPOI[i] += pow(Corr2Poi, 2) * M01Poi;
-            C24EPOI[i] += pow(Corr4Poi, 2) * M0111Poi;
-            Sum22POI[i] += M01Poi / fMultDimuons;
-            Sum24POI[i] += M0111Poi / fMultDimuons;
-          }
-        }
+        Corr22Poi->Fill(fMass, fPt, CentFT0POI, Corr2Poi, M01Poi);
+        Corr24Poi->Fill(fMass, fPt, CentFT0POI, Corr4Poi, M0111Poi);
       }
     }
 
-    // Fill bins for SP and EP
+    // Fill (mass, pt, centrality) bins for SP and EP
     if (!(isnan(fR2SP) || isinf(fR2SP) || isnan(fR2EP) || isinf(fR2EP))) {
-      for (int i = 0; i < NBinsMass; i++) {
-        if (fMass > MassBins[i] && fMass <= MassBins[i + 1]) {
-          V2SP[i] += fU2Q2 / fR2SP;
-          V2ESP[i] += pow(fU2Q2 / fR2SP, 2);
-          V2EP[i] += fCos2DeltaPhi / fR2EP;
-          V2EEP[i] += pow(fCos2DeltaPhi / fR2EP, 2);
-          V2MultBinMass[i] += 1.;
-        }
-      }
+      U2Q2->Fill(fMass, fPt, CentFT0POI, fU2Q2);
+      R2SP->Fill(fMass, fPt, CentFT0POI, fR2SP);
+      Cos2DeltaPhi->Fill(fMass, fPt, CentFT0POI, fCos2DeltaPhi);
+      R2EP->Fill(fMass, fPt, CentFT0POI, fR2EP);
     }
   }
 
@@ -379,14 +308,35 @@ void FlowAnalysis(float dimuonMassMin = 0., float dimuonMassMax = 4.,
     // Scalar-Product & Event-Plane method
     if (V2MultBinMass[i] != 0.) {
       V2SP[i] = V2SP[i] / V2MultBinMass[i];
-      V2ESP[i] = pow(V2ESP[i] / V2MultBinMass[i] - V2SP[i] * V2SP[i], 1. / 2);
+      R2SP[i] = R2SP[i] / V2MultBinMass[i];
+      V2ESP[i] = V2ESP[i] / V2MultBinMass[i];
+      R2ESP[i] = R2ESP[i] / V2MultBinMass[i];
+      V2ESP[i] = pow(V2ESP[i] - V2SP[i] * V2SP[i], 1. / 2);
+      R2ESP[i] = pow(R2ESP[i] - R2SP[i] * R2SP[i], 1. / 2);
+      V2SP[i] = R2SP[i] > 0 ? V2SP[i] / pow(R2SP[i], 1. / 2) : 0.;
+      V2ESP[i] = pow(
+          (V2ESP[i] * V2ESP[i] / R2SP[i] +
+           0.25 * V2SP[i] * V2SP[i] * R2ESP[i] * R2ESP[i] / pow(R2SP[i], 3)) /
+              V2MultBinMass[i],
+          1. / 2);
 
       V2EP[i] = V2EP[i] / V2MultBinMass[i];
-      V2EEP[i] = pow(V2EEP[i] / V2MultBinMass[i] - V2EP[i] * V2EP[i], 1. / 2);
+      R2EP[i] = R2EP[i] / V2MultBinMass[i];
+      V2EEP[i] = V2EEP[i] / V2MultBinMass[i];
+      R2EEP[i] = R2EEP[i] / V2MultBinMass[i];
+      V2EEP[i] = pow(V2EEP[i] - V2EP[i] * V2EP[i], 1. / 2);
+      R2EEP[i] = pow(R2EEP[i] - R2EP[i] * R2EP[i], 1. / 2);
+      V2EP[i] = R2EP[i] > 0 ? V2EP[i] / pow(R2EP[i], 1. / 2) : 0.;
+      V2EEP[i] = pow(
+          (V2EEP[i] * V2EEP[i] / R2EP[i] +
+           0.25 * V2EP[i] * V2EP[i] * R2EEP[i] * R2EEP[i] / pow(R2EP[i], 3)) /
+              V2MultBinMass[i],
+          1. / 2);
+
       if (fPropError) {
         hist_v2SP->SetBinContent(i + 1, V2SP[i]);
         hist_v2EP->SetBinContent(i + 1, V2EP[i]);
-        if (isnan(V2ESP[i])) {
+        if (isnan(V2ESP[i]) || isnan(V2EEP[i])) {
           hist_v2SP->SetBinError(i + 1, 0.);
           hist_v2EP->SetBinError(i + 1, 0.);
         } else {
@@ -407,25 +357,27 @@ void FlowAnalysis(float dimuonMassMin = 0., float dimuonMassMax = 4.,
         C22EPOI[i] = pow(C22EPOI[i] - C22POI[i] * C22POI[i], 1. / 2);
 
         V22POI[i] = C22POI[i] * pow(C22REF, -1. / 2);
-        V22EPOI[i] = pow(C22EPOI[i] * C22EPOI[i] / C22REF +
+        V22EPOI[i] = pow(C22EPOI[i] * C22EPOI[i] / (C22REF * Sum22POI[i]) +
                              0.25 * C22POI[i] * C22POI[i] * C22EREF * C22EREF /
-                                 pow(C22REF, 3),
+                                 (pow(C22REF, 3) * Sum22REFAll),
                          1. / 2);
         V22EPOI[i] = V22EPOI[i] > 0 ? V22EPOI[i] : 0.;
 
         C24POI[i] = C24POI[i] / Sum24POI[i] - 2. * C22POI[i] * C22REF;
         C24EPOI[i] = C24EPOI[i] / Sum24POI[i];
         C24EPOI[i] = pow(C24EPOI[i] - C24POI[i] * C24POI[i], 1. / 2);
-        C24EPOI[i] = pow(C24EPOI[i] * C24EPOI[i] +
-                             4. * C22REF * C22REF * C22EPOI[i] * C22EPOI[i] +
-                             4. * C22POI[i] * C22POI[i] * C22EREF * C22EREF,
-                         1. / 2);
+        C24EPOI[i] = pow(
+            C24EPOI[i] * C24EPOI[i] / Sum24POI[i] +
+                4. * C22REF * C22REF * C22EPOI[i] * C22EPOI[i] / Sum22POI[i] +
+                4. * C22POI[i] * C22POI[i] * C22EREF * C22EREF / Sum22REFAll,
+            1. / 2);
 
         V24POI[i] = -1. * C24POI[i] * pow(-1. * C24REF, -3. / 4);
-        V24EPOI[i] = pow(pow(-1. * C24REF, -6. / 4) * C24EPOI[i] * C24EPOI[i] +
-                             pow(-1. * C24REF, -14. / 4) * C24POI[i] *
-                                 C24POI[i] * C24EREF * C24EREF * 9. / 16,
-                         1. / 2);
+        V24EPOI[i] = pow(
+            pow(-1. * C24REF, -6. / 4) * C24EPOI[i] * C24EPOI[i] / Sum24POI[i] +
+                pow(-1. * C24REF, -14. / 4) * C24POI[i] * C24POI[i] * C24EREF *
+                    C24EREF / Sum24REFAll * 9. / 16,
+            1. / 2);
         if (fPropError) {
           if (isnan(C22EPOI[i])) {
             hist_c22POIMass->SetBinContent(i + 1, C22POI[i]);
